f6d55ecdf71b395987ba55f2d4ffdcb7e76f2701
dmadon <deanna.madon@gmail.com>
2022-06-10 23:48:20 -0500

    generated random password

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,5 @@
 // Assignment code here
 
-
-
-
-
 //PROMPT USER FOR NUMBER OF CHARACTERS IN THE PASSWORD AND RETURN THE RESULT
 var charLength = function(){
   var length=prompt("Choose the number of characters for your password");
@@ -62,20 +58,6 @@ var specialConfirm = function(){
   }
 }//End of specialConfirm function
 
-
-
-
-
-
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var number = [0,1,2,3,4,5,6,7,8,9];
-var special = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
-
-
-
-
-
 // CALL ALL FUNCTIONS FOR PASSWORD LENGTH, LOWER-CASE LETTERS, UPPER-CASE LETTERS, NUMBERS AND SPECIAL CHARACTERS
 
 var passwordLength=(charLength());
@@ -87,33 +69,66 @@ console.log("lower case: "+useLowerCase);
 var useUpperCase=(upperCaseConfirm());
 console.log("upper case: "+useUpperCase);
 
-// var useNumbers=(numberConfirm());
-// console.log("numbers: "+useNumbers);
+var useNumbers=(numberConfirm());
+console.log("numbers: "+useNumbers);
 
-// var useSpecials=(specialConfirm());
-// console.log("special characters: "+useSpecials);
+var useSpecials=(specialConfirm());
+console.log("special characters: "+useSpecials);
+
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var number = [0,1,2,3,4,5,6,7,8,9];
+var special = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
+
+
+
+
+var myArray=[]
+
 
 for (i=1; i<=passwordLength; i++) {
   if (useLowerCase){
     var randomLowerCase = Math.floor(Math.random()*(lowerCase.length));
-    var logLowerCase=(lowerCase[randomLowerCase]);
+    myArray.push(lowerCase[randomLowerCase]);
+    if(myArray.length>=passwordLength){
+      break;
+    }
   }
-  else{
-    var logLowerCase="";
-  }  
+
   if(useUpperCase){
     var randomUpperCase = Math.floor(Math.random()*(upperCase.length));
-    var logUpperCase=(upperCase[randomUpperCase]);
+    myArray.push(upperCase[randomUpperCase]);
+    if(myArray.length>=passwordLength){
+      break;
+    }
   }
-  else{
-  var logUpperCase="";
+
+  if(useNumbers){
+    var randomNumber = Math.floor(Math.random()*(number.length));
+    myArray.push(number[randomNumber]);
+    if(myArray.length>=passwordLength){
+      break;
+    }
   }
-  var string = (logLowerCase+logUpperCase);
-  console.log(string);
+
+  if(useSpecials){
+    var randomSpecial = Math.floor(Math.random()*(special.length));
+    myArray.push(special[randomSpecial]);
+    if(myArray.length>=passwordLength){
+      break;
+    }
+  }
 }
 
 
+var shuffleValues = function(){
+  myArray.sort(()=>Math.random()-0.5);
+  return (myArray.join(''));
+}
 
+var myPassword=shuffleValues();
+
+console.log("myPassword: "+myPassword);
 
 
 
