7778767eae46dc02453ccc15d09b8e0ebb0277f6
dmadon <deanna.madon@gmail.com>
2022-06-13 18:59:05 -0500

    add clarifying comments

--- Develop/script.js
+++ Develop/script.js
@@ -58,6 +58,13 @@ var specialConfirm = function(){
   }
 }//End of specialConfirm function
 
+// DEFINE PASSWORD COMPONENTS: LOWER CASE LETTERS, UPPER CASE LETTERS, NUMBERS AND SPECIAL CHARACTERS
+
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var number = [0,1,2,3,4,5,6,7,8,9];
+var special = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
+
 // CALL ALL FUNCTIONS FOR PASSWORD LENGTH, LOWER-CASE LETTERS, UPPER-CASE LETTERS, NUMBERS AND SPECIAL CHARACTERS
 
 var passwordLength=(charLength());
@@ -75,10 +82,7 @@ console.log("numbers: "+useNumbers);
 var useSpecials=(specialConfirm());
 console.log("special characters: "+useSpecials);
 
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var number = [0,1,2,3,4,5,6,7,8,9];
-var special = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
+
 
 
 
